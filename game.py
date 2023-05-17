f84e892648280b7f28e610b5bed8ffaf0d8b9fb1
Harsh Gill <harsh.gill@yahoo.com>
2023-05-18 00:29:08 +0530

    Added a provision for diagonal combos in state()

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # Implementing Connect Four on CLI
 import random
 import numpy as np
+import time
 
 
 class Game:
@@ -14,6 +15,7 @@ class Board:
         self.position = np.full((rows, cols), "_")
         self.vacant_squares = rows*cols
         self.turn = 'O'
+        self.last_move = 0, 0
 
     def state(self):
         if self.vacant_squares <= 0:
@@ -26,6 +28,17 @@ class Board:
                 if i > 2:
                     if self.position[i][j] == self.position[i-1][j] == self.position[i-2][j] == self.position[i-3][j] != '_':
                         return self.position[i][j]
+        x, y = self.last_move
+        diag = np.diag(self.position, y-x)
+        if diag.size >= 4:
+            for i in range(diag.size-3):
+                if diag[i] == diag[i+1] == diag[i+2] == diag[i+3] != '_':
+                    return diag[i]
+        opp_diag = np.diag(np.rot90(self.position), x+y-self.position.shape[0])
+        if opp_diag.size >= 4:
+            for i in range(diag.size-3):
+                if diag[i] == diag[i+1] == diag[i+2] == diag[i+3] != '_':
+                    return diag[i]
         return 0
 
     def move(self, col):
@@ -36,6 +49,7 @@ class Board:
                     self.vacant_squares -= 1
                     print(f"{self.turn} marked {i},{col}")
                     self.turn = 'X' if self.turn == 'O' else 'O'
+                    self.last_move = i, col
                     return
         self.move(int(input(str(col)+" is an invalid move, try again!")))
 
@@ -62,8 +76,11 @@ def main():
             game.board.move(int(input("Your turn, pick a column(0-7)!")))
             print(game.board.position)
             current_state = game.board.state()
+            if current_state != 0:
+                break
         if game.board.turn == 'X':
             game.board.rand()
+            time.sleep(0.75)
             print(game.board.position)
             current_state = game.board.state()
     if current_state == 1:
